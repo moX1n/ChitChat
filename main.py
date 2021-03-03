3d3485d2d43d82659d693c109914e4c49410780d
Ezekiel O'Brien <zekeyobrien@gmail.com>
2021-03-04 01:36:33 +1000

    Completed rough copy of ChitChat

--- main.py
+++ main.py
@@ -15,16 +15,21 @@ overwrite=False, filename="cooks", wait_time=3)
 def chat_options():
     while True:
         desired_string = input("Enter desired string: \n>")
-        str.desired_range = input("How many times to spam it? \n>")
-        desired_time = input("How long between messages? \n>")
-        for i in range(desired_range):
+        desired_range = int(input("How many times to spam it? \n>"))
+        desired_time = int(input("How long between messages? \n>"))
+        for i in range(0, desired_range):
             search = driver.find_element_by_class_name("ScInputBase-sc-1wz0osy-0")
-            search.send_keys(desired_string)
+            search.send_keys(desired_string + " " + str(i))
             search.send_keys(Keys.RETURN)
+            time.sleep(desired_time)
             print(i)
 
-            
-            
+def chat_check():
+    print("Initializing... MrDestructoid")
+    search = driver.find_element_by_class_name("ScInputBase-sc-1wz0osy-0")
+    search.send_keys(Keys.RETURN)
+    time.sleep(2)
+    search = driver.find_element_by_class_name("ScCoreButtonPrimary-sc-1qn4ixc-1").click()
             
             
             
@@ -32,10 +37,11 @@ def chat_options():
 def main():
     
     loaded_cookies = cookie_handler.load_cookies()
-    time.sleep(2)
+    time.sleep(1)
     driver.refresh()
     print("Refreshed Twitch Chat")
-    time.sleep(2)
+    time.sleep(1)
+    chat_check()
     chat_options()
 
 main()
