3ea7382c6e56be1c5ea296522ba24c1da4bfa73f
Ezekiel O'Brien <zekeyobrien@gmail.com>
2021-03-03 12:31:25 +1000

    Started work on main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-a
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+print("What's the name of the channel would you like the bot to operate in?\n")
+print("Make sure you type it correctly!")
+selectedChannel = input(">")
+
+driver = webdriver.Firefox()
+driver.get("https://www.twitch.tv/popout/{}/chat?popout=".format(selectedChannel))
+print("Connecting to: " + driver.title + ".tv/" + selectedChannel)
